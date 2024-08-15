70df01200d142126ef5a5e3431ebe76812f17172
Artem Glebov <artem.glebov@gmail.com>
2024-08-15 13:42:11 +0100

    Better type annotations and docstrings in the caching module

--- code/utils/caching.py
+++ code/utils/caching.py
@@ -17,13 +17,36 @@ cache_dir = Path('.')
 
 
 def get_path(entry_name: str, expected_type: type) -> Path:
+    """Construct path for persisting an entry of a given type
+
+    Parameters
+    ----------
+    entry_name: str
+        name of the entry
+    expected_type: type
+        type of the entry
+
+    Returns
+    -------
+    Path
+        path to the file on disk
+    """
     if expected_type is np.ndarray:
         return cache_dir / f'{entry_name}.npy'
     else:
         return cache_dir / entry_name
 
 
-def save_obj(entry_name, obj):
+def save_obj(entry_name: str, obj: Any):
+    """Persist entry on disk
+
+    Parameters
+    ----------
+    entry_name: str
+        name of the entry
+    obj: Any
+        entry object to persist
+    """
     filepath = get_path(entry_name, type(obj))
     logger.debug('Writing %s', filepath)
     if isinstance(obj, np.ndarray):
@@ -37,7 +60,21 @@ def save_obj(entry_name, obj):
             pickle.dump(obj, f)
 
 
-def read_obj(entry_name, expected_type: type):
+def read_obj(entry_name: str, expected_type: type) -> Any:
+    """Read persisted entry from disk
+
+    Parameters
+    ----------
+    entry_name: str
+        name of the entry
+    expected_type: type
+        type of the entry
+
+    Returns
+    -------
+    Any
+        entry object read from disk
+    """
     filepath = get_path(entry_name, expected_type)
     logger.debug('Reading %s', filepath)
     if expected_type is np.ndarray:
@@ -51,18 +88,18 @@ def read_obj(entry_name, expected_type: type):
             return pickle.load(f)
 
 
-def calculate_cached(
-    calculation: Callable[[], np.ndarray],
+def calculate_cached[T](
+    calculation: Callable[[], T],
     entry_name: str,
     recalculate: bool = False,
     save: bool = False,
     expected_type: type = np.ndarray,
-) -> np.ndarray:
+) -> T:
     """Perform expensive calculation or retrieve cached result
 
     Parameters
     ----------
-    calculation: Callable[[], np.ndarray]
+    calculation: Callable[[], T]
         calculation to perform
     filepath: Path
         path to cache the result of the calculation
@@ -75,7 +112,7 @@ def calculate_cached(
 
     Returns
     -------
-    np.ndarray
+    T
         result of the calculation
     """
     filepath = get_path(entry_name, expected_type)
@@ -132,8 +169,30 @@ def map_cached(
     return list(mapper(calculate, items))
 
 
-def cached(recalculate=False, persist=True, filename_gen=None, memory_cache_maxsize=None):
-    """Decorator adding caching on disk to functions"""
+def cached[T](
+        recalculate: bool = False,
+        persist: bool = True,
+        filename_gen: Optional[Callable[[str, int], str]] = None,
+        memory_cache_maxsize: Optional[int] = None,
+) -> Callable[[Callable[..., T]], Callable[..., T]]:
+    """Decorator adding caching on disk to functions
+
+    Parameters
+    ----------
+    recalculate: bool
+        if True, perform the calculation from scratch ignoring any cached results
+    persist: bool
+        if True, persist recalculated result on disk (only has effect if `recalculate` is `True`)
+    filename_gen: Optional[Callable[[str, int], str]]
+        function returning file name to use when persisting results on disk
+    memory_cache_maxsize: Optional[int]
+        maximum number of results to retain in memory cache
+
+    Returns
+    -------
+    Callable[[Callable[..., T]], Callable[..., T]]
+        decorator function
+    """
     if filename_gen is None:
         def filename_gen(func_name, *args, **kwargs):
             assert len(kwargs) == 0, 'kwargs not supported'
@@ -152,8 +211,34 @@ def cached(recalculate=False, persist=True, filename_gen=None, memory_cache_maxs
     return decorator
 
 
-def cached_batch(*, item_type, recalculate=False, persist=True, filename_gen=None, memory_cache_maxsize=None):
-    """Decorator adding caching on disk to functions that batch calculation for multiple items"""
+def cached_batch[T](
+        *,
+        item_type: type,
+        recalculate: bool = False,
+        persist: bool = True,
+        filename_gen: Optional[Callable[[str, int], str]] = None,
+        memory_cache_maxsize: Optional[int] = None,
+) -> Callable[[Callable[[], Iterable[T]]], Callable[[int], T]]:
+    """Decorator adding caching on disk to functions that batch calculation for multiple items
+
+    Parameters
+    ----------
+    item_type: type
+        type of items in the batch
+    recalculate: bool
+        if True, perform the calculation from scratch ignoring any cached results
+    persist: bool
+        if True, persist recalculated result on disk (only has effect if `recalculate` is `True`)
+    filename_gen: Optional[Callable[[str, int], str]]
+        function returning file name to use when persisting results on disk
+    memory_cache_maxsize: Optional[int]
+        maximum number of results to retain in memory cache
+
+    Returns
+    -------
+    Callable[[Callable[[], Iterable[T]]], Callable[[int], T]]
+        decorator function
+    """
     if filename_gen is None:
         def filename_gen(func_name, i):
             return f'{func_name}_{i}'
@@ -169,10 +254,10 @@ def cached_batch(*, item_type, recalculate=False, persist=True, filename_gen=Non
                 end_time = time.time()
                 logger.info('Calculation time for %s: %f s', func.__name__, end_time - start_time)
                 if persist:
-                    for j in range(len(batch)):
+                    for j, item in enumerate(batch):
                         entry_name = filename_gen(func.__name__, j)
                         logger.debug(f'Persisting calculation result: {entry_name}')
-                        save_obj(entry_name, batch[j])
+                        save_obj(entry_name, item)
                 res = batch[i]
             else:
                 logger.debug('Reading from disk cache: %s', entry_name)
@@ -182,8 +267,30 @@ def cached_batch(*, item_type, recalculate=False, persist=True, filename_gen=Non
     return decorator
 
 
-def subscriptable(func=None, *, n=None):
-    """Decorator to use indexing on a function"""
+def subscriptable(
+        func: Optional[Callable] = None,
+        *,
+        n: Optional[int] = None,
+):
+    """Decorator to use indexing on a function
+
+    For a function taking one argument:
+    ```
+    @subscriptable
+    def func(i):
+        ...
+    ```
+    the decorator returns an object that can indexed:
+    ```
+    func[5]
+    ```
+    which translates into an invocation
+    ```
+    func(5)
+    ```
+
+    This decorator can be used either with the `n` argument or without arguments.
+    """
     if func is None:
         # decorator is called with the size parameter
         assert n is not None, 'n must be provided'
