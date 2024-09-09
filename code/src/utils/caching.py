c1bf59baf7abd31a3d7fb12ebbd60dea4c1f43ee
Artem Glebov <artem.glebov@gmail.com>
2024-09-09 11:59:35 +0100

    Simplify caching utilities

--- code/src/utils/caching.py
+++ code/src/utils/caching.py
@@ -14,6 +14,7 @@ import jax.numpy as jnp
 
 logger = logging.getLogger(__name__)
 cache_dir = Path('.')
+lru_cache_max_size = None
 
 
 def get_path(entry_name: str, expected_type: type) -> Path:
@@ -88,153 +89,99 @@ def read_obj(entry_name: str, expected_type: type) -> Any:
             return pickle.load(f)
 
 
-def calculate_cached[T](
-    calculation: Callable[[], T],
-    entry_name: str,
-    recalculate: bool = False,
-    persist: bool = False,
-    read_only: bool = False,
-    expected_type: type = np.ndarray,
-) -> T:
-    """Perform expensive calculation or retrieve cached result
+class CacheFunc:
 
-    Parameters
-    ----------
-    calculation: Callable[[], T]
-        calculation to perform
-    filepath: Path
-        path to cache the result of the calculation
-    recalculate: bool
-        recalculate the results from scratch if True, use the cached result if False
-    persist: bool
-        persist the result of the calculation
-    read_only: bool
-        if True, only attempt to read the cached item from dist, never recalculate
-    expected_type: type
-        expected type of the result when retrieving from file
+    def __init__(
+            self,
+            func: Callable[[], Iterable],
+            item_type: type,
+            recalculate: bool,
+            persist: bool,
+            read_only: bool,
+            batch: bool,
+            batch_size: Optional[int] = None,
+            filename_gen: Optional[Callable[[str, int], str]] = None,
+    ):
+        assert not (recalculate and read_only), 'Cannot use recalculate and read_only together'
+        assert not batch or item_type is not None, 'Item type must be provided in batch mode'
+        self._func = func
+        self._item_type = item_type or func.__annotations__['return']
+        self._recalculate = recalculate
+        self._persist = persist
+        self._read_only = read_only
+        self._batch = batch
+        self._batch_size = batch_size
+        self._filename_gen = filename_gen
+        
 
-    Returns
-    -------
-    T
-        result of the calculation
-    """
-    assert not (read_only and recalculate), 'read_only and recalculate cannot be True simultaneously'
+    def recalculate(self, *args, persist: bool = True):
+        if self._batch:
+            if len(args) != 1:
+                raise ValueError('index argument expected in the batch mode')
+            i = args[0]
+            if self._batch_size is not None and (i < 0 or i >= self._batch_size):
+                raise IndexError('item index out of range')
+            logger.info('Recalculating batch: %s', self._func.__name__)
+            start_time = time.time()
+            batch = self._func()
+            end_time = time.time()
+            logger.info('Calculation time for %s: %f s', self._func.__name__, end_time - start_time)
+            if persist:
+                for j, item in enumerate(batch):
+                    entry_name = self._filename_gen(self._func.__name__, j)
+                    logger.debug(f'Persisting calculation result: {entry_name}')
+                    save_obj(entry_name, item)
+            return batch[i]
+        else:
+            entry_name = self._filename_gen(self._func.__name__, *args)
+            logger.info('Recalculating: %s', entry_name)
+            start_time = time.time()
+            res = self._func(*args)
+            end_time = time.time()
+            logger.info('Calculation time for %s: %f s', entry_name, end_time - start_time)
+            if persist:
+                logger.debug(f'Persisting calculation result: {entry_name}')
+                save_obj(entry_name, res)
+            return res
 
-    filepath = get_path(entry_name, expected_type)
-    if read_only or (filepath.exists() and not recalculate):
-        logger.debug('Reading from disk cache: %s', entry_name)
-        res = read_obj(entry_name, expected_type)
-    else:
-        logger.info('Recalculating: %s', entry_name)
-        start_time = time.time()
-        res = calculation()
-        end_time = time.time()
-        logger.info('Calculation time for %s: %f s', entry_name, end_time - start_time)
-        if persist:
-            logger.debug(f'Persisting calculation result: {entry_name}')
-            save_obj(entry_name, res)
+    @lru_cache(lru_cache_max_size)
+    def __call__(self, *args):
+        entry_name = self._filename_gen(self._func.__name__, *args)
+        filepath = get_path(entry_name, self._item_type)
+        if self._read_only or (filepath.exists() and not self._recalculate):
+            logger.debug('Reading from disk cache: %s', entry_name)
+            res = read_obj(entry_name, self._item_type)
+        else:
+            res = self.recalculate(*args, persist=self._persist)
 
-    return res
+        return res
 
+    def __getitem__(self, i: int):
+        return self.__call__(i)
 
-def map_cached(
-    func: Callable[[Any], np.ndarray],
-    items: Iterable[Any],
-    filepath_gen: Callable[[Any], Path],
-    recalculate: bool = False,
-    save: bool = False,
-    mapper: Callable[[Callable[[Any], np.ndarray], Iterable[Any]], Iterable[np.ndarray]] = map,
-    expected_type: type = np.ndarray,
-):
-    """Perform expensive calculation for provided items or read stored results
+    def __setitem__(self, key, val):
+        raise NotImplementedError
 
-    Parameters
-    ----------
-    func: Callable[[Any], np.ndarray]
-        function to apply to every item
-    items: Iterable[Any]
-        items to map
-    filepath_gen: Callable[[Any], Path]
-        function returning the path for cache file
-    recalculate: bool
-        recalculate the results from scratch if True, use the cached result if False
-    save: bool
-        save the result of the calculation (only has effect if `recalculate` is True)
-    mapper: Callable[[Callable[[Any], np.ndarray], Iterable[Any]], Iterable[np.ndarray]]
-        mapper function to use (defaults to the standard `map` function)
-    expected_type: type
-        expected type of the result when retrieving from file
+    def __delitem__(self, key):
+        raise NotImplementedError
 
-    Returns
-    -------
-    list[Any]
-        result of calculation for each item
-    """
-    def calculate(item):
-        return calculate_cached(lambda: func(item), filepath_gen(item), recalculate, save)
-    return list(mapper(calculate, items))
+    def __len__(self):
+        return self._batch_size
+
+    def __iter__(self):
+        for i in range(self._batch_size):
+            yield self.__call__(i)
 
 
 def cached[T](
-        recalculate: bool = False,
-        persist: bool = True,
-        read_only: bool = False,
-        filename_gen: Optional[Callable[[str, int], str]] = None,
-        memory_cache_maxsize: Optional[int] = None,
-) -> Callable[[Callable[..., T]], Callable[..., T]]:
-    """Decorator adding caching on disk to functions
-
-    Parameters
-    ----------
-    recalculate: bool
-        if True, perform the calculation from scratch ignoring any cached results. Default: False
-    persist: bool
-        if True, persist recalculated result on disk (only has effect if `recalculate` is `True`). Default: True
-    read_only: bool
-        if True, read the cached item from disk and never attempt to recalculate. Default: False
-    filename_gen: Optional[Callable[[str, int], str]]
-        function returning file name to use when persisting results on disk
-    memory_cache_maxsize: Optional[int]
-        maximum number of results to retain in memory cache
-
-    Returns
-    -------
-    Callable[[Callable[..., T]], Callable[..., T]]
-        decorator function
-    """
-    if filename_gen is None:
-        def filename_gen(func_name, *args, **kwargs):
-            assert len(kwargs) == 0, 'kwargs not supported'
-            if len(args) > 0:
-                return func_name + '_' + '_'.join([str(arg) for arg in args])
-            else:
-                return func_name
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            entry_name = filename_gen(func.__name__, *args, **kwargs)
-            expected_type = func.__annotations__['return']
-            calculation = lambda: func(*args, **kwargs)
-            return calculate_cached(
-                calculation=calculation,
-                entry_name=entry_name,
-                recalculate=recalculate,
-                persist=persist,
-                read_only=read_only,
-                expected_type=expected_type,
-            )
-        return lru_cache(memory_cache_maxsize)(wrapper)
-    return decorator
-
-
-def cached_batch[T](
         *,
-        item_type: type,
+        item_type: Optional[type] = None,
         recalculate: bool = False,
         persist: bool = True,
         read_only: bool = False,
         filename_gen: Optional[Callable[[str, int], str]] = None,
-        memory_cache_maxsize: Optional[int] = None,
+        batch: bool = False,
+        batch_size: Optional[int] = None,
 ) -> Callable[[Callable[[], Iterable[T]]], Callable[[int], T]]:
     """Decorator adding caching on disk to functions that batch calculation for multiple items
 
@@ -250,8 +197,10 @@ def cached_batch[T](
         if True, read the cached batch from disk and never attempt to recalculate. Default: False
     filename_gen: Optional[Callable[[str, int], str]]
         function returning file name to use when persisting results on disk
-    memory_cache_maxsize: Optional[int]
-        maximum number of results to retain in memory cache
+    batch: bool
+        if True, the underlying calculation returns a sequence of items as opposed to invidivual items. Default: False
+    batch_size: Optional[int]
+        size of the batch to use in range checks (only has effect if `batch` is `True`)
 
     Returns
     -------
@@ -259,102 +208,22 @@ def cached_batch[T](
         decorator function
     """
     if filename_gen is None:
-        def filename_gen(func_name, i):
-            return f'{func_name}_{i}'
-    def decorator(func):
-        assert not (recalculate and read_only), 'Cannot use recalculate and read_only together'
-
-        def do_recalculate(persist: bool = True):
-            logger.info('Recalculating batch: %s', func.__name__)
-            start_time = time.time()
-            batch = func()
-            end_time = time.time()
-            logger.info('Calculation time for %s: %f s', func.__name__, end_time - start_time)
-            if persist:
-                for j, item in enumerate(batch):
-                    entry_name = filename_gen(func.__name__, j)
-                    logger.debug(f'Persisting calculation result: {entry_name}')
-                    save_obj(entry_name, item)
-            return batch
-
-        @wraps(func)
-        def wrapper(i):
-            entry_name = filename_gen(func.__name__, i)
-            filepath = get_path(entry_name, item_type)
-            if read_only or (filepath.exists() and not recalculate):
-                logger.debug('Reading from disk cache: %s', entry_name)
-                res = read_obj(entry_name, item_type)
+        def filename_gen(func_name, *args, **kwargs):
+            assert len(kwargs) == 0, 'kwargs not supported'
+            if len(args) > 0:
+                return func_name + '_' + '_'.join([str(arg) for arg in args])
             else:
-                res = do_recalculate(persist)[i]
-
-            return res
-
-        wrapper.recalculate = do_recalculate
-
-        return lru_cache(memory_cache_maxsize)(wrapper)
+                return func_name
+    def decorator(func):
+        cache_func = CacheFunc(
+            func=func,
+            item_type=item_type,
+            recalculate=recalculate,
+            persist=persist,
+            read_only=read_only,
+            batch=batch,
+            batch_size=batch_size,
+            filename_gen=filename_gen,
+        )
+        return wraps(func)(cache_func)
     return decorator
-
-
-def subscriptable(
-        func: Optional[Callable] = None,
-        *,
-        n: Optional[int] = None,
-):
-    """Decorator to use indexing on a function
-
-    For a function taking one argument:
-    ```
-    @subscriptable
-    def func(i):
-        ...
-    ```
-    the decorator returns an object that can indexed:
-    ```
-    func[5]
-    ```
-    which translates into an invocation
-    ```
-    func(5)
-    ```
-
-    This decorator can be used either with the `n` argument or without arguments.
-    """
-    if func is None:
-        # decorator is called with the size parameter
-        assert n is not None, 'n must be provided'
-        def decorator(func):
-            class SubscriptableFuncion:
-                def __init__(self, func):
-                    self.func = func
-                def __call__(self, *args, **kwargs):
-                    return self.func(*args, **kwargs)
-                def __getitem__(self, key):
-                    return self.func(key)
-                def __setitem__(self, key, value):
-                    raise NotImplementedError
-                def __delitem__(self, key):
-                    raise NotImplementedError
-                def __len__(self):
-                    return n
-                def __iter__(self):
-                    for i in range(n):
-                        yield self(i)
-            return wraps(func)(SubscriptableFuncion(func))
-        return decorator
-    else:
-        # decorator is called without the size parameter
-        assert n is None
-        class SubscriptableFuncion:
-            def __init__(self, func):
-                self.func = func
-            def __call__(self, *args, **kwargs):
-                return self.func(*args, **kwargs)
-            def __getitem__(self, key):
-                return self.func(key)
-            def __setitem__(self, key, value):
-                raise NotImplementedError
-            def __delitem__(self, key):
-                raise NotImplementedError
-            def __len__(self):
-                return n
-        return wraps(func)(SubscriptableFuncion(func))
