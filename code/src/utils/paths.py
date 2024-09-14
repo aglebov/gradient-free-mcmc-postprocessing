ddd668d627082502dc14917b94e967f8b818ec6e
Artem Glebov <artem.glebov@gmail.com>
2024-09-14 14:19:24 +0000

    Use S3 for caching in Lotka-Volterra notebooks

--- code/src/utils/paths.py
+++ code/src/utils/paths.py
@@ -3,3 +3,5 @@ from pathlib import Path
 ROOT_PATH = (Path(__file__).parent / '..' / '..' / '..').resolve()
 GENERATED_DATA_PATH = ROOT_PATH / 'data' / 'generated'
 FIGURES_PATH = ROOT_PATH / 'report' / 'figures'
+
+S3_BUCKET_NAME = 'gradient-free-mcmc-postprocessing'
